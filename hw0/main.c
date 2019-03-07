dd74d3b2080d6afd860061348f593c4bc572cd29
lishi <zeldaspirit@gmail.com>
2019-03-07 07:09:36 +0000

    Changes to be committed: 	modified:   hw0/main.c 	modified:   hw0/map.c 	modified:   hw0/wc.c
     Untracked files:
    	hw0/Makefile
    	hw0/main
    	hw0/map
    	hw0/tags
    	hw0/test.txt
    	hw0/test1.txt
    	hw0/wc
    	session1/

--- hw0/main.c
+++ hw0/main.c
@@ -3,8 +3,16 @@
 
 int main() {
     struct rlimit lim;
-    printf("stack size: %ld\n", 0L);
-    printf("process limit: %ld\n", 0L);
-    printf("max file descriptors: %ld\n", 0L);
+    getrlimit(RLIMIT_STACK, &lim);
+    rlim_t OL = lim.rlim_cur;
+    printf("stack size: %ld\n", OL);
+
+    getrlimit(RLIMIT_NPROC, &lim);
+    OL = lim.rlim_cur;
+    printf("process limit: %ld\n", OL);
+
+    getrlimit(RLIMIT_NOFILE, &lim);
+    OL = lim.rlim_cur;
+    printf("max file descriptors: %ld\n", OL);
     return 0;
 }
